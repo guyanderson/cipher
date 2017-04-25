a0d6199b5457f0186602da85f2dbf9fa0aa247e5
guy <luckythelich@hotmail.com>
2017-04-24 18:48:49 -0700

    created function(?) to grab first and last character of user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 var saySomething = function(whatToSay) {
   alert(whatToSay);
-};
+};                //basic function that creates a customised alert.
 
 var sentence = prompt("Enter a sentence.");
-sentence;
+sentence;    //prompt for user to enter a sentence that will be saved as a variable called "sentence'."
 
-saySomething(sentence);
+var firstChar = sentence.charAt(0); //stores the first caracter of the users sentence.
+
+var lastChar = sentence.charAt(sentence.length-1); // stores the first caracter of the users sentence.
+
+saySomething(lastChar);
